a79c82f46c74eb2df2a3a73b7b581b0e750452e7
vutagwa <m2vutagwa@gmail.com>
2023-06-10 12:25:31 +0300

    all changes

---
+++ src/myname/Myname.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myname;
+import java.util.Scanner;
+/**
+ *
+ * @author marion
+ */
+public class Myname {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner (System.in);
+        System.out.println("what is your integer");
+        int num = input.nextInt();
+        System.out.println(num+1);
+        
+    }
+    
+}
